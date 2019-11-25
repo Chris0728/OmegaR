4c9461532e572f54c5674817df2641db6d5e58d1
robinpmac <robinpmac@gmail.com>
2019-11-25 13:28:27 -0800

    Edited again again

--- app/src/main/java/com/example/omegar/MealInput.java
+++ app/src/main/java/com/example/omegar/MealInput.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,10 +20,21 @@ public class MealInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_input2);
 
+        // Get a reference to the AutoCompleteTextView in the layout
+        AutoCompleteTextView foodNameInput = findViewById(R.id.autoCompleteTextView2);
+        // Get the string array
+        String[] countries = getResources().getStringArray(R.meal_names.countries_array);
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        foodNameInput.setAdapter(adapter);
+
+        EditText foodWeightInput = findViewById(R.id.editText6);
+
         final Button mealInputButton = findViewById(R.id.input_meal); //change to button ID
         mealInputButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+
             }
         });
 
@@ -32,8 +45,8 @@ public class MealInput extends AppCompatActivity {
             }
         });
 
-        EditText foodWeightInput = findViewById(R.id.editText6);
-        
+
+
 
 
     }
