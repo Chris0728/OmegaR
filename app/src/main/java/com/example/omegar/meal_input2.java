6b9ef2edf5d2caa08f1c1a8a0f3607da1abe58de
dami1998 <43226387+dami1998@users.noreply.github.com>
2019-11-25 12:56:44 -0800

    meal input 2

---
+++ app/src/main/java/com/example/omegar/meal_input2.java
@@ -0,0 +1,14 @@
+package com.example.omegar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class meal_input2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_input2);
+    }
+}
