79054f9026ff976247146f39ed7aacae47ae3d39
robinpmac <robinpmac@gmail.com>
2019-11-25 12:54:10 -0800

    Edited MealInput.java

--- app/src/main/java/com/example/omegar/MealInput.java
+++ app/src/main/java/com/example/omegar/MealInput.java
@@ -1,14 +1,35 @@
 package com.example.omegar;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MealInput extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meal_input);
+        setContentView(R.layout.activity_meal_input2);
+
+        final Button mealInputButton = findViewById(R.id.input_meal); //change to button ID
+        mealInputButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+        final Button backButton = findViewById(R.id.back); //change to button ID
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+            }
+        });
+
+
     }
+
+
 }
