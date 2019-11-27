275f0628ebbce04601087833b34f4c7dfee92b9b
Chris0728 <43189330+Chris0728@users.noreply.github.com>
2019-11-26 20:48:03 -0800

    added education content

--- app/src/main/java/com/example/omegar/Education.java
+++ app/src/main/java/com/example/omegar/Education.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Education extends AppCompatActivity {
 
+    private TextView text;
     private Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
+
+        text = findViewById(R.id.sampleText);
 
 
         backBtn = findViewById(R.id.back);
